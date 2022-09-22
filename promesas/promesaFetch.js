d3d0ac2152ebd12231fe1cbefc89b1aad1c3414f
Daniel Silverio <daniel.silverio@mariachi.io>
2022-09-22 18:42:10 -0500

    Se agrego el codigo para realizar peticiones a una api con fetch

--- promesas/promesaFetch.js
+++ promesas/promesaFetch.js
@@ -4,15 +4,47 @@ const url = "https://pokeapi.co/api/v2/pokemon/ditto";
 const url2 = "https://rickandmortyapi.com/api/character/235";
 
 function getData(url) {
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      return data;
-    })
-    .catch((error) => {
-      return error;
-    });
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        resolve(data);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
 }
 
-const data = getData(url);
-console.log("Get Data ", data);
+getData(url)
+  .then((data) => {
+    console.log("Data ", data);
+  })
+  .catch((error) => {
+    console.log("Error ", error);
+  });
+
+getData(url2)
+  .then((data) => {
+    console.log("Data ", data);
+  })
+  .catch((error) => {
+    console.log("Error ", error);
+  });
+
+
+// Con la herramienta Promise.all puedes lanzar varias promesas al mismo tiempo
+// y esperar una sola respuesta con toda la informacion en un arreglo
+// const promesaPokemon = getData(url);
+// const promesaRAndM = getData(url2);
+// const promesas = [promesaPokemon, promesaRAndM];
+
+// Promise.all(promesas)
+//   .then((response) => {
+//     const [pokemon, personajeRM] = response;
+//     console.log("pokemon ", pokemon);
+//     console.log("personajeRM ", personajeRM);
+//   })
+//   .catch((error) => {
+//     console.log("error ", error);
+//   });
